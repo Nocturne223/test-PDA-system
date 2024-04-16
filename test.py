6e3f2ca3319ff277c4e7b1d153f13ec6070fb914
Albert James <e.albert223@gmail.com>
2024-04-16 11:58:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,7 @@ import keras
 import streamlit as st
 import numpy as np
 from PIL import Image
-from keras.models import load_model
 
-
-model1 = load_model('../official-models/LettuceModel.h5')  # saved model from training
-model2 = load_model('../official-models/CauliflowerModel.h5')  # saved model from training
-model3 = load_model('../official-models/SugarcaneModel-1.h5')  # saved model from training
-model4 = load_model('../official-models/PepperModel.h5')  # saved model from training
 
 Lettuce_names = ["lettuce_BacterialLeafSpot", "lettuce_BotrytisCrownRot", "lettuce_DownyMildew", "lettuce_Healthy"]
 
@@ -20,42 +14,6 @@ Sugarcane_names = ["sugarcane_Healthy", "sugarcane_Mosaic", "sugarcane_RedRot", 
 Pepper_names = ["pepper_Healthy", "pepper_CercosporaLeafSpot", "pepper_Fusarium", "pepper_Leaf_Curl"]
 
 folder_path = "saved_images"
-
-
-def preprocess_image(image_path):
-    img = Image.open(image_path)
-    img = img.resize((256, 256))  # Resize image to match model's input shape
-    img_array = np.array(img) / 255.0  # Normalize pixel values to range [0, 1]
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    return img_array
-
-def L_predict_disease(image_path):
-    preprocessed_img = preprocess_image(image_path)
-    prediction = model1.predict(preprocessed_img)
-    disease_index = np.argmax(prediction)  # Get the index of the predicted Lettuce class
-    disease_class = Lettuce_names[disease_index] # Fetch the class name using the index
-    return disease_class
-
-def C_predict_disease(image_path):
-    preprocessed_img = preprocess_image(image_path)
-    prediction = model2.predict(preprocessed_img)
-    disease_index = np.argmax(prediction)  # Get the index of the predicted Cauliflower class
-    disease_class = Cauliflower_names[disease_index] # Fetch the class name using the index
-    return disease_class
-
-def S_predict_disease(image_path):
-    preprocessed_img = preprocess_image(image_path)
-    prediction = model3.predict(preprocessed_img)
-    disease_index = np.argmax(prediction)  # Get the index of the predicted Sugarcane class
-    disease_class = Sugarcane_names[disease_index] # Fetch the class name using the index
-    return disease_class
-
-def P_predict_disease(image_path):
-    preprocessed_img = preprocess_image(image_path)
-    prediction = model4.predict(preprocessed_img)
-    disease_index = np.argmax(prediction)  # Get the index of the predicted Pepper class
-    disease_class = Pepper_names[disease_index] # Fetch the class name using the index
-    return disease_class
 
 
 # =======================================
@@ -102,30 +60,30 @@ with tab1:
             elif select == 'Lettuce':
                 # predicting Lettuce disease
                 image_path = plantpic
-                predicted_class = L_predict_disease(image_path)
-                pred1 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred1)
+                # predicted_class = L_predict_disease(image_path)
+                # pred1 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred1)
                 
             elif select == 'Cauliflower':
                 # predicting Cauliflower disease
                 image_path = plantpic
-                predicted_class = C_predict_disease(image_path)
-                pred2 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred2)
+                # predicted_class = C_predict_disease(image_path)
+                # pred2 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred2)
                 
             elif select == 'Sugarcane':
                 # predicting Sugarcane disease
                 image_path = plantpic
-                predicted_class = S_predict_disease(image_path)
-                pred3 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred3)
+                # predicted_class = S_predict_disease(image_path)
+                # pred3 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred3)
                 
             elif select == 'Pepper':
                 # predicting Pepper disease
                 image_path = plantpic
-                predicted_class = P_predict_disease(image_path)
-                pred4 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred4)
+                # predicted_class = P_predict_disease(image_path)
+                # pred4 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred4)
                
             
     elif pick == 'Upload':
@@ -143,30 +101,30 @@ with tab1:
             elif select == 'Lettuce':
                 # predicting Lettuce disease
                 image_path = plantpic
-                predicted_class = L_predict_disease(image_path)
-                pred1 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred1)
+                # predicted_class = L_predict_disease(image_path)
+                # pred1 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred1)
                 
             elif select == 'Cauliflower':
                 # predicting Cauliflower disease
                 image_path = plantpic
-                predicted_class = C_predict_disease(image_path)
-                pred2 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred2)
+                # predicted_class = C_predict_disease(image_path)
+                # pred2 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred2)
                 
             elif select == 'Sugarcane':
                 # predicting Sugarcane disease
                 image_path = plantpic
-                predicted_class = S_predict_disease(image_path)
-                pred3 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred3)
+                # predicted_class = S_predict_disease(image_path)
+                # pred3 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred3)
                 
             elif select == 'Pepper':
                 # predicting Pepper disease
                 image_path = plantpic
-                predicted_class = P_predict_disease(image_path)
-                pred4 = "Predicted Disease Class: " + predicted_class
-                st.image(plantpic,pred4)
+                # predicted_class = P_predict_disease(image_path)
+                # pred4 = "Predicted Disease Class: " + predicted_class
+                # st.image(plantpic,pred4)
                 
     
 
